072f5897a07855d11896b9f56a345cd14468f5c9
Matheus Vinicius <matheusv83200@gmail.com>
2024-08-05 21:35:46 -0300

    :recycle: refactor: nome das funções

--- features/steps/login_steps.py
+++ features/steps/login_steps.py
@@ -4,25 +4,25 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 @given(u'que o usuário está na página de login')
-def step_impl(context):
+def page_login(context):
     title = context.browser.title
     assert "VALIDA APP" in title, "Não está na página de login"
 
 @when(u'informar o login e a senha')
-def step_impl(context):
+def page_login_submit_user_password(context):
     username = "admin"
     password = "admin"
     context.browser.find_element(By.NAME, "username").send_keys(username)
     context.browser.find_element(By.NAME, "password").send_keys(password)
 
 @then(u'o sistema deve autenticar o usuário')
-def step_impl(context):
+def authenticate_user_login(context):
     context.browser.find_element(By.XPATH, "//button[contains(@class,'btn btn-primary')]").submit()
     
 
 
 @then(u'permitir o acesso às funcionalidades do sistema.')
-def step_impl(context):
+def page_admin(context):
     wait = WebDriverWait(context.browser,10)
     page_admin = wait.until(EC.visibility_of_element_located((By.XPATH, "//h1[contains(.,'Bem vindo ao Desafio')]")))
     assert "Bem vindo ao Desafio" in page_admin.text, "Não está na página do administrador"
